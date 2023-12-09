118cc1003e1302264366279314bd007c3a0e1ae6
Gokul190902 <gokul190902@gmail.com>
2023-12-09 21:39:55 +0530

    Spend Calculator initial push

---
+++ src/main/java/com/be/spendCalculator/SpendCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.be.spendCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendCalculatorApplication.class, args);
+	}
+
+}
